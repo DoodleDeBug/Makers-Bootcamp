0e1315bfb35b820713ac76f2ed72b4a152eb12f0
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-10-25 12:02:33 +0100

    edit code after code review and add better README

--- week 1/airport_challenge/lib/airport.rb
+++ week 1/airport_challenge/lib/airport.rb
@@ -12,6 +12,23 @@ class Airport
 
   end
 
+  def land(plane)
+    if full?
+      "This airport is full - cannot land"
+    else
+      planes.push(plane)
+      "Landed"
+    end
+  end
+
+  def take_off(plane)
+    if planes.include?(plane)
+      "Took off"
+    else
+      "This plane is not at the airport"
+    end
+  end
+
   def full?
     planes.length >= capacity
   end
--- week 1/airport_challenge/spec/airport_spec.rb
+++ week 1/airport_challenge/spec/airport_spec.rb
@@ -3,6 +3,25 @@ require_relative './../lib/airport'
 
 describe Airport do
 
+  it "lands a plane at an airport" do
+    plane = Plane.new
+    expect(subject.land(plane)).to eq "Landed"
+  end
+
+  it "plane can take off from an airport" do
+    airport = Airport.new(5)
+    plane = Plane.new
+
+    airport.land(plane)
+    expect(airport.take_off(plane)).to eq "Took off"
+  end
+
+  it "does not land plane if the airport is full" do
+    plane = Plane.new
+    subject.land(plane)
+    expect(subject.land(plane)).to eq "This airport is full - cannot land"
+  end
+
   it 'has a default capacity' do
     expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
   end
@@ -10,8 +29,15 @@ describe Airport do
   it 'has a variable capacity' do
     airport = Airport.new(50)
     plane = Plane.new
-    50.times { plane.land(airport) }
-    expect(plane.land(airport)).to eq "This airport is full - cannot land"
+    50.times { airport.land(plane) }
+    expect(airport.land(plane)).to eq "This airport is full - cannot land"
+  end
+
+  it 'does not let plane take_off from airport if it is not there' do
+    airport = Airport.new(5)
+    plane = Plane.new
+
+    expect(airport.take_off(plane)).to eq "This plane is not at the airport"
   end
 
 end
