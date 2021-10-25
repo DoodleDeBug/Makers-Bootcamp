bce77e90a5c42a897c80ec136ba5e23d9f0b067c
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-10-25 17:09:19 +0100

    refactor

--- week 2/oystercard/lib/oystercard.rb
+++ week 2/oystercard/lib/oystercard.rb
@@ -1,20 +1,16 @@
 class Oystercard
-  DEFAULT_BALANCE = 0
   LIMIT = 90
   
   attr_reader :balance, :limit
 
   def initialize
-    @balance = DEFAULT_BALANCE
+    @balance = 0
     @limit = LIMIT
   end
 
   def top_up(amount)
-    if balance + amount > limit
-      raise "Cannot top up anymore - you will exceed your limit of #{limit}"
-    else
-      @balance = balance + amount
-    end
+    fail "Cannot top up anymore - you will exceed your limit of #{LIMIT}" if amount + balance > LIMIT
+    @balance += amount
   end
 
 end
