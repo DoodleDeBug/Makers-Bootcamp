e687f7467836df39e7d3d46c0526f4c46c9f1b15
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-10-25 17:03:37 +0100

    implement max top_up limit on oyster)

--- week 2/oystercard/lib/oystercard.rb
+++ week 2/oystercard/lib/oystercard.rb
@@ -1,14 +1,20 @@
 class Oystercard
   DEFAULT_BALANCE = 0
+  LIMIT = 90
   
-  attr_reader :balance
+  attr_reader :balance, :limit
 
-  def initialize(balance=DEFAULT_BALANCE)
-    @balance = balance
+  def initialize
+    @balance = DEFAULT_BALANCE
+    @limit = LIMIT
   end
 
   def top_up(amount)
-    @balance = balance + amount
+    if balance + amount > limit
+      raise "Cannot top up anymore - you will exceed your limit of #{limit}"
+    else
+      @balance = balance + amount
+    end
   end
 
 end
--- week 2/oystercard/spec/oystercard_spec.rb
+++ week 2/oystercard/spec/oystercard_spec.rb
@@ -10,4 +10,10 @@ describe Oystercard do
     card.top_up(10)
     expect(card.balance).to eq(10)
   end
+
+  it 'raises error if balance would exceed limit' do
+    card = Oystercard.new
+    subject.limit.times {card.top_up(1)} 
+    expect {card.top_up(1)}.to raise_error("Cannot top up anymore - you will exceed your limit of #{subject.limit}")
+  end
 end
