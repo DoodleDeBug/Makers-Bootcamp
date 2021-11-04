0d9894362874cff0338d87fb7440d6e2d72300da
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-11-04 10:31:28 +0000

    get birthday-msg page working

--- week 3/birthday_app/app.rb
+++ week 3/birthday_app/app.rb
@@ -6,4 +6,13 @@ get '/' do
   erb(:index)
 end
 
+post '/birthday-msg' do
+  p params
+  @name = params[:name]
+  @day = params[:day]
+  @month = params[:month]
+  
+  erb(:birthday)
+end
+
 # http://localhost:4567
