396243db54fccb2b63819c1b52fa4f7ba5a23869
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-11-05 14:59:48 +0000

    create feature test file - commented out for now until I can figure out how to use it)

---
+++ week 3/birthday_app/spec/features/greet_spec.rb
@@ -0,0 +1,10 @@
+# feature 'greet:' do
+
+#   scenario 'get confirmation of birthday' do
+#     enter_info_and_submit_form
+
+#     expect(page).to have_content 'You made it another year Bob!'
+#   end
+
+
+# end
