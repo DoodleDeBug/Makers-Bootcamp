9dfbcc9af90a26adc49b8fad92e49b69c68816da
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-11-15 08:40:58 +0000

    refactor

--- week 4/chitter-challenge/spec/features/post_peep_spec.rb
+++ week 4/chitter-challenge/spec/features/post_peep_spec.rb
@@ -1,8 +1,9 @@
+require_relative 'web_helpers'
+
 feature 'posting a peep' do
 
   scenario '"new peep" button brings up a form' do
-    visit '/'
-    click_on 'New Peep'
+    visit_home_and_select_new_peep
 
     within('form') { 
       expect(page).to have_content 'Your Peep:'
@@ -11,9 +12,9 @@ feature 'posting a peep' do
 
   end
 
-  scenario 'my peep shows up on homepage when i hit "post peep" button' do
-    visit '/'
-    click_on 'New Peep'
+  scenario 'my peep shows up on recent peeps page when i hit "post peep" button' do
+    visit_home_and_select_new_peep
+
     fill_in 'peep', with: 'This is my first peep!'
     click_button 'Post Peep'
     expect(page).to have_content 'This is my first peep!'
@@ -22,11 +23,10 @@ feature 'posting a peep' do
   # scenario 'peep includes user handle' do
     
   # end
+
   # scenario 'peep includes users name' do
     
   # end
-  # scenario 'peep includes text' do
-    
-  # end
+
 
 end
