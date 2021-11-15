26c5e1ee8eea7daff1c19437281d3451c8e44710
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-11-15 13:27:40 +0000

    Attach a database to a web application

--- week 4/BookmarkManager/spec/bookmark_spec.rb
+++ week 4/BookmarkManager/spec/bookmark_spec.rb
@@ -1,13 +1,18 @@
 require './lib/bookmark'
 
 describe Bookmark do
-  it 'returns a list of bookmarks' do
-    bookmarks = Bookmark.all
-    expect(bookmarks).to include "http://makersacademy.tech"
-    expect(bookmarks).to include "http://www.google.com"
-    expect(bookmarks).to include "http://www.destroyallsoftware.com"
 
-  end
+  describe '.all' do
+    it 'returns a list of bookmarks' do
 
+      bookmarks = Bookmark.all
+
+      expect(bookmarks).to include 'http://www.makersacademy.com'
+      expect(bookmarks).to include "http://www.google.com"
+      expect(bookmarks).to include "http://www.twitter.com"
+      expect(bookmarks).to include "http://www.facebook.com/"
+
+    end
+end
 
 end
--- week 4/BookmarkManager/spec/features/view_bookmark_spec.rb
+++ week 4/BookmarkManager/spec/features/view_bookmark_spec.rb
@@ -2,8 +2,10 @@ feature 'view bookmark' do
   scenario 'displays bookmarks' do
     visit '/bookmarks'
     expect(page).to have_content 'Here are your bookmarks:'
-    expect(page).to have_content "http://makersacademy.tech"
-    expect(page).to have_content "http://www.google.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
+
+    expect(page).to have_content 'http://www.makersacademy.com'
+      expect(page).to have_content "http://www.google.com"
+      expect(page).to have_content "http://www.twitter.com"
+      expect(page).to have_content "http://www.facebook.com/"
   end
 end
