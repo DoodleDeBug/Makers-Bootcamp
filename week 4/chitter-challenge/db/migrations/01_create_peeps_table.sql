218b69a0951a13797c42fb6eb39f8db173a12cec
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-11-21 16:19:24 +0000

    add timestamp colum to table

--- week 4/chitter-challenge/db/migrations/01_create_peeps_table.sql
+++ week 4/chitter-challenge/db/migrations/01_create_peeps_table.sql
@@ -3,3 +3,6 @@ CREATE TABLE peeps(id SERIAL PRIMARY KEY, peep VARCHAR(60), name VARCHAR(60), us
 INSERT INTO peeps (peep,name,username) VALUES ('peep 1', 'bob', 'bobthebuilder123');
 INSERT INTO peeps (peep,name,username) VALUES ('another peep', 'adam', '3gg');
 INSERT INTO peeps (peep,name,username) VALUES ('last peep for now', 'sara', 'sarah123');
+
+ALTER TABLE peeps ADD COLUMN timestamp TIMESTAMP;
+
