524ac2ded424bf1b16621f039fa41b3a287dd977
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-11-30 10:19:30 +0000

    remove unused files

--- week 5/bowling-challenge-ruby/lib/frame.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Frame
-  def initialize
-  end
-end
--- week 5/bowling-challenge-ruby/spec/spec_helper.rb
+++ week 5/bowling-challenge-ruby/spec/spec_helper.rb
@@ -16,8 +16,6 @@ SimpleCov.formatter =
   )
 SimpleCov.start
 
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
